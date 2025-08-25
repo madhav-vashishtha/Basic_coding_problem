a0016c2464bf74e235d9782132de7649036fbefa
Madhav Vashishtha <Madhav@Anjalis-MacBook-Air.local>
2025-08-25 23:07:36 +0530

    solved day of the week

---
+++ Day_of_the_week.py
@@ -0,0 +1,17 @@
+n= int(input("enter a number (1-7):"))
+if n == 1:
+    print("SUNDAY")
+elif n == 2:
+    print("MONDAY")
+elif n == 3:
+    print("TUESDAY")
+elif n == 4:
+    print("WEDNESDAY")
+elif n == 5:
+    print("THURSDAY")
+elif n == 6:
+    print("FRIDAY")
+elif n == 7:
+    print("SATURDAY")
+else:
+    print("INVALID DAY")
