b7abe6ac48ced8ed75b3c78e7920c54f831e7145
madhav-vashishtha <madhavvash1105@gmail.com>
2025-09-14 01:57:57 +0530

    traffic signal program

---
+++ Traffic_signal _system.py
@@ -0,0 +1,10 @@
+color=input("enter signal color: ")
+
+if color=="red":
+    print("STOP")
+elif color=="yellow":
+    print("Get Ready")
+elif color=="green":
+    print("GO")
+else:
+    print("invalid signal")
