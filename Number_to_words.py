553efc69c334b0bbda3e8b08d1c31bfec697801b
madhav-vashishtha <madhavvash1105@gmail.com>
2025-09-14 12:42:45 +0530

    number to words program

---
+++ Number_to_words.py
@@ -0,0 +1,22 @@
+num=int(input("enter a number (1-9): "))
+
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+elif num == 3:
+    print("Three")
+elif num == 4:
+    print("Four")
+elif num == 5:
+    print("Five")
+elif num == 6:
+    print("Six")
+elif num == 7:
+    print("Seven")
+elif num == 8:
+    print("Eight")
+elif num == 9:
+    print("Nine")
+else:
+    print("Out of Range")
